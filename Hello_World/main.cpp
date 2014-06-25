575ebd109c472788b3cda146d9c8a2fca4cdded1
shannonswong <shannon.s.wong@gmail.com>
2014-06-24 23:55:37 -0700

    Signed-off-by: shannonswong <shannon.s.wong@gmail.com>

---
+++ Hello_World/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:  Hello World Homework
+ * Author: Shannon Wong
+ * Purpose: To See If I am Competent Enough for the Class
+ * Created on June 24, 2014, 11:30 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout<<"Hello World!!!"<<endl;
+//wow i did it
+    return 0;
+}
+
