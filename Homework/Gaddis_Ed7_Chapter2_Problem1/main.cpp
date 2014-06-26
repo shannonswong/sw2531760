c2c7295d646b895e577f4cda1ed3d59fa7cb1f5b
shannonswong <shannon.s.wong@gmail.com>
2014-06-25 21:03:16 -0700

    Signed-off-by: shannonswong <shannon.s.wong@gmail.com>

---
+++ Homework/Gaddis_Ed7_Chapter2_Problem1/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   Gaddis_Ed7_Chapter2_Problem12
+ * Author: Shannon Wong
+ * Purpose: Diamond Pattern
+ * Created on June 25, 2014, 8:46 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/* 
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"   *"<<endl;
+    cout<<"  ***"<<endl;
+    cout<<" *****"<<endl;
+    cout<<"*******"<<endl;
+    cout<<" *****"<<endl;
+    cout<<"  ***"<<endl;
+    cout<<"   *"<<endl;
+    return 0;
+}
+
