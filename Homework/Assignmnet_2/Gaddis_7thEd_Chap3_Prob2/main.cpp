343c591c5c62d520c9f312d6e7faac39e8e33376
shannonswong <shannon.s.wong@gmail.com>
2014-06-30 20:40:59 -0700

    Signed-off-by: shannonswong <shannon.s.wong@gmail.com>

---
+++ Homework/Assignmnet_2/Gaddis_7thEd_Chap3_Prob2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on June 30, 2014, 8:30 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
