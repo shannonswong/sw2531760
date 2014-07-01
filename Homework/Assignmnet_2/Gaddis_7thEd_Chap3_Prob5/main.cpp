0934b9a8c0966ba954af31dcf420e3ec9e082d79
shannonswong <shannon.s.wong@gmail.com>
2014-06-30 21:21:27 -0700

    Signed-off-by: shannonswong <shannon.s.wong@gmail.com>

--- Homework/Assignmnet_2/Gaddis_7thEd_Chap3_Prob5/main.cpp
+++ Homework/Assignmnet_2/Gaddis_7thEd_Chap3_Prob5/main.cpp
@@ -1,18 +1,43 @@
 /* 
- * File:   main.cpp
- * Author: rcc
- *
+ * File:   Gaddis_7thEd_Chap3_Prob5
+ * Author: Shannon Wong
+ * Purpose: Batting Average
  * Created on June 30, 2014, 9:10 PM
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
-/*
+/* //User Defined Libraries
  * 
+ * //Global Constants
+ * 
+ * //Function Prototypes
+ * 
+ * //Executing is like a box of chocolates.
  */
+
 int main(int argc, char** argv) {
+
+//Declare Variables
+    double times, hits;
+    
+//Input the Data
+    cout<<"Times At Bat: ";
+    cin>>times;
+    
+    cout<<"Number of Hits: ";
+    cin>>hits;
+    
+//Calculate the Data
+    cout.setf(ios::fixed);
+    cout.precision(4);
+    double average = hits / times;
+
+//Output the Data
+    cout<<"Batting Average: "<<average<<endl;
 
     return 0;
 }
