3cc284dd216e7d2fc19f7713033deb8b7cb7e690
shannonswong <shannon.s.wong@gmail.com>
2014-07-06 18:17:10 -0700

    Signed-off-by: shannonswong <shannon.s.wong@gmail.com>

---
+++ Projects/Gaddis_7thEd_Chap4_Prob1/main.cpp
@@ -0,0 +1,42 @@
+/* 
+ * File:   Gaddis_7thEd_Chap4_Prob1
+ * Author: Shannon Wong
+ * Purpose: Minimum/Maximum
+ * Created on July 6, 2014, 5:44 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/* //User Defined Libraries
+ * 
+ * //Global Constants
+ * 
+ * //Function Prototypes
+ * 
+ * //Execution
+ */
+
+int main(int argc, char** argv) {
+    
+//Declare Variables
+    int num1, num2;
+    
+//Prompt User to Input Two Integers 
+    cout<<"Enter two integers. "<<endl; 
+    cin>>num1>>num2;
+
+//Which is smaller and which is larger?
+    if (num1 > num2)
+        cout<<num1<<" is larger than "<<num2<<"."<<endl;
+    else if (num2 > num1)
+        cout<<num2<<" is larger and than "<<"."<<endl;
+   
+    
+    
+
+    return 0;
+}
+
