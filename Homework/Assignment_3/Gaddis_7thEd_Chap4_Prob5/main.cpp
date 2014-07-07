ab5ea1a70b3c6f9faded8f5f8bb4ff760139f7fa
shannonswong <shannon.s.wong@gmail.com>
2014-07-06 20:04:37 -0700

    Signed-off-by: shannonswong <shannon.s.wong@gmail.com>

---
+++ Homework/Assignment_3/Gaddis_7thEd_Chap4_Prob5/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   Gaddis_7thEd_Chap4_Prob5
+ * Author: Shannon Wong
+ * Purpose: Book Club Points
+ * Created on July 6, 2014, 7:54 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/* //User Defined Libraries
+ * 
+ * //Global Constants
+ * 
+ * //Function Prototypes
+ * 
+ * //Execution
+ */
+
+int main(int argc, char** argv) {
+ 
+//Declare Variables
+    int books;
+
+//Prompt User to Input Number of Books Purchased
+    cout<<"How many books have you purchased this month?"<<endl;
+    cin>>books;
+    
+//Books to Points Output
+    if (books == 0)
+        cout<<"Points Earned: 0";
+    else if (books == 1)
+        cout<<"Points Earned: 5";
+    else if (books == 2)
+        cout<<"Points Earned: 15";
+    else if (books == 3)
+        cout<<"Points Earned: 30";
+    else if (books >= 4)
+        cout<<"Points Earned: 60"; 
+
+    return 0;
+}
+
