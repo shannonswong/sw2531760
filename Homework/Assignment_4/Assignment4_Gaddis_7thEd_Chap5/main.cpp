2b59a6239e1961200f906e2bc9ff635fe70cc587
shannonswong <shannon.s.wong@gmail.com>
2014-07-11 23:24:51 -0700

    Signed-off-by: shannonswong <shannon.s.wong@gmail.com>

--- Homework/Assignment_4/Assignment4_Gaddis_7thEd_Chap5/main.cpp
+++ Homework/Assignment_4/Assignment4_Gaddis_7thEd_Chap5/main.cpp
@@ -7,6 +7,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -15,6 +16,8 @@ using namespace std;
  * //Global Constants
  * 
  * //Function Prototypes 
+ * 
+ * //Execution Begins!!!!
  */
 
 int main(int argc, char** argv) {
@@ -27,13 +30,13 @@ int main(int argc, char** argv) {
         cout<<"Type 0 for Gaddis Chapter 5 Problem 1"<<endl;
         cout<<"Type 1 for Gaddis Chapter 5 Problem 2"<<endl;
         cout<<"Type 2 for Gaddis Chapter 5 Problem 3"<<endl;
-        cout<<"Type 3 for Gaddis Chapter 5 Problem 4"<<endl;
-        cout<<"Type 4 for Gaddis Chapter 5 Problem 5"<<endl;
-        cout<<"Type 5 for Gaddis Chapter 5 Problem 6"<<endl;
-        cout<<"Type 6 for Gaddis Chapter 5 Problem 7"<<endl;
-        cout<<"Type 7 for Gaddis Chapter 5 Problem 8"<<endl;
-        cout<<"Type 8 for Gaddis Chapter 5 Problem 9"<<endl;
-        cout<<"Type 9 for Gaddis Chapter 5 Problem 10"<<endl;
+        cout<<"Type 3 for Gaddis Chapter 5 Problem 5"<<endl;
+        cout<<"Type 4 for Gaddis Chapter 5 Problem 6"<<endl;
+        cout<<"Type 5 for Gaddis Chapter 5 Problem 7"<<endl;
+        cout<<"Type 6 for Gaddis Chapter 5 Problem 8"<<endl;
+        cout<<"Type 7 for Gaddis Chapter 5 Problem 4"<<endl;
+        cout<<"Type 8 for Gaddis Chapter 5 Problem 12"<<endl;
+        cout<<"Type 9 for Gaddis Chapter 5 Problem 19"<<endl;
         cin>>choice;
     
     
@@ -94,25 +97,160 @@ int main(int argc, char** argv) {
                 }  
                 break;         
             }case '3':{
-                cout<<"Problem B here!"<<endl<<endl;
+            //Problem 5: Speed Conversion Chart
+            //Declare Variables
+                float kph;
+                float convrate = 0.621371; //conversion rate
+
+                //Chart
+                cout << "   KPH          MPH\n";
+                cout << "-------------------------\n";
+
+                //Produce Results 
+                     for (kph = 60; kph <=130; kph +=5)
+                        cout << kph <<"\t" << kph*convrate << endl;
                 break;
             }case '4':{
-                cout<<endl;
+            //Problem 6: Ocean Levels
+            //Declare Variables
+                float speed, distance;
+                int time;
+
+                do{
+                    //Prompt User to Input Values
+                    cout<<"Vehicle Speed"<<endl;
+                    cin>>speed;
+                    cout<<"Time Traveled"<<endl;
+                    cin>>time;
+                }while(speed<0||time<0);
+                //Calculate 
+                distance = time * speed;
+
+
+                //Output
+                cout<<fixed<<setprecision(2);
+                //Chart
+                        cout<<"hours \t\t distance traveled"<<endl;
+                cout<<"-----------------------------------------------------"<<endl;
+
+                for(int i=1; i<=time; i++){
+                    cout<<i<<" \t\t "<<i*speed<<endl;
+                     }
                 break;
             }case '5':{
-                cout<<endl;
+            //Problem 7: Pennies for Pay
+            //Declare Variables
+                float days, amount=0.01f;
+
+                //Input Values
+                do{
+                    cout<<"Enter Number of Days. Maximum is 60"<<endl;
+                    cin>>days;
+                }while(days<0||days>60);
+
+                //Chart
+                cout<<"days\t\tpay amount"<<endl;
+                cout<<"---------------------------------------------"<<endl;
+                cout<<fixed<<setprecision(2);
+                //Calculate and Output Data
+                for(int i=1; i<=days; i++){
+                    cout<<i<<"\t\t$"<<amount<<endl;
+                    amount *= 2;
+                    }
                 break; 
             }case '6':{
-                cout<<endl;
+            //Problem 8: Calories Burned
+            //Declare Variables
+                int floors, rooms=0, occupied=0;
+
+                //Input Data
+                do{
+                    cout<<"How many floors?"<<endl;
+                    cin>>floors;
+                }while(floors<1);
+                for(int i=1; i<=floors; i++){
+                    int tempRoom, tempOcc;
+                    do{
+                        cout<<"Floor "<<i<<":  How many rooms? "<<endl;
+                        cin>>tempRoom;
+                    }while(tempRoom<10);
+                    rooms += tempRoom;
+                    do{
+                        cout<<"Floor "<<i<<":  How many are occupied? "<<endl;
+                        cin>>tempOcc;
+                    }while(tempOcc>tempRoom);
+                    occupied += tempOcc;
+                }
+
+                //Calculate
+                float percent = occupied*100.0f/rooms;
+                cout<<"total floors\ttotal rooms\toccupied\tpercentage coccupied"<<endl;
+                cout<<floors<<"\t\t"<<rooms<<"\t\t"<<occupied<<"\t\t"<<percent<<"%"<<endl;
                 break; 
             }case '7':{
-                cout<<endl;
+            //Problem 4: Celsius to Fahrenheit Table 
+            //Declare Variables
+                float celsius, fahrenheit; 
+
+            //Input Data
+                cout<<"Enter Temperature in Celsius: ";
+                cin>>celsius;
+
+            //Output Data
+                cout<<"Degrees In Celsius\t\t Degrees In Fahrenheit "<<endl;
+                cout<<"____________________________________________________"<<endl;
+                  for(int i=celsius; i<=celsius+20; i++){
+                      fahrenheit = i * (9.0/5.0) + 32;
+                      cout<<i<<"\t\t\t\t"<<fahrenheit<<endl;
+                  }
                 break; 
             }case '8':{
-                cout<<endl;
+            //Problem 12: The Greatest and Least of These
+            //Declare Variables
+                int number,small, large, temp; 
+
+                cout<<"Enter an Integer"<<endl;
+                cin>>number;
+                small =number;
+                large = number;
+
+                while(number!=-99){
+                    cout<<"enter a number"<<endl;
+                    cin>>number;
+                    if (number!=-99){
+                        if (number<small){
+                            small = number;
+                        }
+                        else if (number>large){
+                            large = number;
+                        }        
+                    }      
+                }
+                cout<<"Largest #  = "<<large<<"\tSallest number =  "<<small<<endl;
+        
                 break; 
             }case '9':{
-                cout<<endl;
+            //Problem 19: Managing Budget
+            //Declare values
+                float budget, counter; //budget input. counter is expenses
+                float total;
+            //Input Budget
+                cout<<"What is this month's total budget? "<<endl;
+                cin>>budget;
+
+                while (counter!=-9999)
+                {
+                    total+=counter;
+                    cout<<"Enter Expense.  Press -9999 to Stop"<<endl;
+                    cin>>counter;
+
+                }
+                if (total> budget){
+                    cout<<"You have gone over budget.  Total amount is   "<<total<<endl;
+                }
+                else {
+                    cout<<"Congratulations, you are under budget.  Total amount is  "<<total<<endl;
+                }
                 break;     
             }default:{
                 cout<<"Exit Menu"<<endl<<endl;
