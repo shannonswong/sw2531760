99955868c7baaaea72faf1fb1fbb42156f5977f5
shannonswong <shannon.s.wong@gmail.com>
2014-07-28 00:56:59 -0700

    Signed-off-by: shannonswong <shannon.s.wong@gmail.com>

---
+++ Homework/Assignment_6/Gaddis_7thEd_Chap8_Assignment6_Menu/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: CiCi
+ *
+ * Created on July 27, 2014, 7:07 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
