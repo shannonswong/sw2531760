33b559326993ced5b4ab157fde07737500f5cab2
shannonswong <shannon.s.wong@gmail.com>
2014-07-30 17:41:17 -0700

    Signed-off-by: shannonswong <shannon.s.wong@gmail.com>

--- Projects/Project2/main.cpp
+++ Projects/Project2/main.cpp
@@ -22,21 +22,35 @@ int getA(); //scene A
 void def(int); //default
 int choice1a(); //choice 1 scene A
 int choice2a(); //choice 2 scene A
-int choice3a(); //choice 3  A
-int choice4a(); 
+int choice3a(); //choice 3 A
+int choice4a();
 int sceneB(); //scene B
 int getB(); //scene B
 void def(int); //default
 int choice1b(); //choice 1 scene B
 int choice2b(); //choice 2 scene B
-int choice3b(); //choice 3  B
+int choice3b(); //choice 3 B
 int sceneC(); //scene C
 int getC(); //scene C
-void def(int); //default
 int choice1c(); //choice 1 scene C
 int choice2c(); //choice 2 scene C
-int choice3c(); //choice 3  C
-
+int dicegame(); //dice game function
+int choice3c(); //choice 3 C
+int sceneD(); //scene D
+int getD(); //scene D
+int choice1d(); //choice 1 scene D
+int choice2d(); //choice 2 scene D
+int choice3d(); //choice 3 D
+int sceneE(); //scene E
+int getE(); //scene E
+int choice1e(); //choice 1 scene E
+int choice2e(); //choice 2 scene E
+int choice3e(); //choice 3 E
+int sceneF(); //scene F
+int getF(); //scene F
+int choice1f(); //choice 1 scene F
+int choice2f(); //choice 2 scene F
+int choice3f(); //choice 3 F
 int youwin(); //the end
 int gameover(); //game over
 
@@ -85,26 +99,36 @@ int inC;
             case 3: choice3c();break; //choice 3 scene C
             default:;
         };
-////scene D
-//int inD;
-//        sceneD();
-//        inD=getD();
-//        switch(inD){
-//            case 1: choice1d();break; //choice 1 scene D
-//            case 2: choice2d();break; //choice 2 scene D
-//            case 3: choice3d();break; //choice 3 scene D
-//            default:;
-//        };
-////scene E
-//int inE;
-//        sceneE();
-//        inE=getE();
-//        switch(inE){
-//            case 1: choice1e();break; //choice 1 scene E
-//            case 2: choice2e();break; //choice 2 scene E
-//            case 3: choice3e();break; //choice 3 scene E
-//            default:;
-//        };
+//scene D
+int inD;
+        sceneD();
+        inD=getD();
+        switch(inD){
+            case 1: choice1d();break; //choice 1 scene D
+            case 2: choice2d();break; //choice 2 scene D
+            case 3: choice3d();break; //choice 3 scene D
+            default:;
+        };
+//scene E
+int inE;
+        sceneE();
+        inE=getE();
+        switch(inE){
+            case 1: choice1e();break; //choice 1 scene E
+            case 2: choice2e();break; //choice 2 scene E
+            case 3: choice3e();break; //choice 3 scene E
+            default:;
+        };
+//scene F
+int inF;
+        sceneF();
+        inE=getF();
+        switch(inF){
+        case 1: choice1f();break; //choice 1 scene F
+        case 2: choice2f();break; //choice 2 scene F
+        case 3: choice3f();break; //choice 3 scene F
+        default:;
+ };
     return 0;
 }
 
@@ -331,17 +355,32 @@ int choice1c(){
 
 int choice2c(){
 //Declare variables
-    
+    unsigned short answer;
+    char anything;
 //choice2c story
     cout<<"You chose the door in the middle. It is locked but there is a"
             "\nnote that tells you to head back to the desk and"
             "\nsee the papers for a way to open the door."<<endl;
     cout<<"The paper says: If you can solve this simple math problem,\n"
             "you'll be able to continue on your journey out of here."<<endl;
-    cout<<"";
-            
-            
+    cout<<"What is: 100 + 200?"<<endl;
+    cin>>answer;
+    cout<<"You go to the door with your answer."<<endl;
+    cout<<"There is a blank panel. You trace "<<answer<<" on it."<<endl;
+    if (answer==300){
+        cout<<"Although you can't see how it happened, the door suddenly"
+                "clicks and opens magically."<<endl;
+        cout<<"Type anything to continue."<<endl;
+        cin>>anything; 
+        anything = sceneD();
+    }
+    else if (answer != 300){ 
+        cout<<"Incorrect. Type anything to continue."<<endl;
+        cin>>anything;
+        anything = sceneC();        
+    }
 }
+    
 int choice3c(){
   char anything;
     int enter;
@@ -362,6 +401,208 @@ int choice3c(){
     }
     else cout<<"Invalid choice."<<endl;  
 }
+
+int getD(){
+        int inD;
+        cin>>inD;
+        return inD;
+}
+
+int sceneD(){
+    cout<<"You manage to enter the next room. Good job."<<endl;
+    cout<<"Now you go to choose one of the three doors, as you"
+            "did before."<<endl;
+    cout<<"--------------CHOICES--------------"<<endl;
+    cout<<"[TYPE] 1: door on the left."<<endl;
+    cout<<"[TYPE] 2: door in the middle."<<endl;
+    cout<<"[TYPE] 3: door on the right."<<endl;
+}
+
+int choice1d(){
+    char roll;
+    cout<<"You approach the door and dice magically emerge"
+            "from the center of it."<<endl;
+    cout<<"A voice says: You chose wisely. However, being wise won't"
+            "be to your advantage in this game."<<endl;
+    cout<<"If you manage to roll a more even sums than odd, then"
+            "you may proceed."<<endl;
+    cout<<"Go ahead and roll the die. (Don't worry they'll stay floating."<<endl;
+    cin>>roll;
+    roll = dicegame(); 
+}
+
+int choice2d(){
+    char anything;
+    int enter;
+    cout<<"You chose the door on the right. It opens but it's just an"
+            "\empty closet."<<endl;
+    cout<<"Type anything to continue."<<endl;
+    cin>>anything;
+     cout<<"--------------CHOICES--------------"<<endl;
+    cout<<"[TYPE] 1: door on the left."<<endl;
+    cout<<"[TYPE] 2: door in the middle."<<endl;
+    cout<<"---"<<endl;
+    cin>>enter;
+    if (enter==1){
+        choice1d();
+    }
+    else if (enter==2){
+        choice2d();
+    }
+    else cout<<"Invalid choice."<<endl;
+}
+
+int  choice3d(){
+    char anything;
+    int enter;
+    cout<<"You chose the door in the middle. It opens but it's just an"
+            "\empty closet."<<endl;
+    cout<<"Type anything to continue."<<endl;
+    cin>>anything;
+    cout<<"--------------CHOICES--------------"<<endl;
+    cout<<"[TYPE] 1: door on the left."<<endl;
+    cout<<"----"<<endl;
+    cout<<"[TYPE] 3: door on the right."<<endl;
+    cin>>enter;
+    if (enter==1){
+        choice1d();
+    }
+    else if (enter==3){
+        choice3d();
+    }
+    else cout<<"Invalid choice."<<endl;
+}
+
+int dicegame(){
+//Declare variables
+int wins=0, loss=0;
+for(int game=1;game<=5;game++){
+        //Roll dice
+        char die1=rand()%6+1;
+        char die2=rand()%6+1;
+        char sum1=die1+die2;
+        switch(sum1){
+            case 2:case 4:case 6: case 8: case 10: case 12:{
+                wins++;break;
+            }default:{
+               loss++;break;
+            }
+        }
+    }
+    //Output the results
+    cout<<"Number of Games won = "<<wins<<endl;
+    cout<<"Number of Games lost = "<<loss<<endl;
+    //Outcome
+    if (wins>loss){
+     cout<<"Congratulations. You may proceed."<<endl;
+     cout<<"Type anything to proceed."<<endl;
+     char anything;
+     cin>>anything;
+     anything = sceneE();
+    }
+    else if (loss>wins){
+        cout<<"Sorry. You did not win."<<endl;
+        cout<<"You must return to the previous room."<<endl;
+     cout<<"Type anything to proceed."<<endl;
+     char anything2;
+     cin>>anything2;
+     anything2 = sceneD();
+    }   
+}
+
+int getE(){
+        int inE;
+        cin>>inE;
+        return inE;
+}
+
+int sceneE(){
+    cout<<"Good job. You made it past two doors."<<endl;
+    cout<<"I wonder what the next game will be."<<endl;
+    cout<<"--------------CHOICES--------------"<<endl;
+    cout<<"[TYPE] 1: door on the left."<<endl;
+    cout<<"[TYPE] 2: door in the middle."<<endl;
+    cout<<"[TYPE] 3: door on the right."<<endl;
+}
+
+int choice1e(){
+    char anything;
+    int enter;
+    cout<<"You chose the door on the left. It opens but it's just an"
+            "\empty closet."<<endl;
+    cout<<"Type anything to continue."<<endl;
+    cin>>anything;
+    cout<<"--------------CHOICES--------------"<<endl;
+    cout<<"--------"<<endl;
+    cout<<"[TYPE] 2: door in the middle."<<endl;
+    cout<<"[TYPE] 3: door on the right."<<endl;
+    cin>>enter;
+    if (enter==2){
+        choice2e();
+    }
+    else if (enter==3){
+        choice3e();
+    }
+    else cout<<"Invalid choice."<<endl;
+}
+
+int choice2e(){
+    char anything;
+    int enter;
+    cout<<"You chose the door in the middle. It opens but it's just an"
+            "\empty closet."<<endl;
+    cout<<"Type anything to continue."<<endl;
+    cin>>anything;
+   cout<<"--------------CHOICES--------------"<<endl;
+    cout<<"[TYPE] 1: door on the left."<<endl;
+    cout<<"--------"<<endl;
+    cout<<"[TYPE] 3: door on the right."<<endl;
+    cin>>enter;
+    if (enter==1){
+        choice1e();
+    }
+    else if (enter==3){
+        choice3e();
+    }
+    else cout<<"Invalid choice."<<endl; 
+}
+
+int choice3e(){
+    float answer; 
+    char anything;
+    cout<<"You choose the door on the right."<<endl;
+    cout<<"You approach the door and a voice congratulates you."<<endl;
+    cout<<"Ah, you chose wisely once again! Now you must answer another"
+            " problem that will be slightly tougher than the first one."<<endl;
+    cout<<"Determine the kinetic energy of a 625-kg roller "
+            " coaster car that is moving with a speed of 18.3 m/s."<<endl;
+    //Calculate answer to  check
+    float ke = 0.5*625*(18.3^2);
+    cin>>answer; 
+    if (answer==1.05e5){
+        cout<<"The answer is "<<ke<<" and you entered "<<answer<<"which"
+                " happens to be correct."<<endl;
+        cout<<"Type anything to continue."<<endl;
+        cin>>anything; 
+        anything = sceneF();
+    }
+    else if (answer != 1.05e5){ 
+        cout<<"Incorrect. Type anything to continue."<<endl;
+        cin>>anything;
+        anything = sceneF();        
+}
+}
+  
+int getF(){
+        int inF;
+        cin>>inF;
+        return inF;
+}    
+
+int sceneF(){
+    
+}
+    
 
 //The End--You Win
 int youwin(){
